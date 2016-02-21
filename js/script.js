d35fa4fedfc2bbe41f4b7cca37337075de19b284
1heart <yixin1996@gmail.com>
2016-02-20 20:32:56 -0500

    Fixed navbar problem

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 
+var isDesktop = window.innerWidth >= 768
+var navbarPos = isDesktop ? $("#navbar-main").position().top : 0;
 $(window).bind('scroll', function(e) {
-  var isDesktop = window.innerWidth >= 768
-  var navbarPos = isDesktop ? $("#navbar-main").position().top : 0;
+  console.log(navbarPos);
   if ($(window).scrollTop() > navbarPos) {
     $('#navbar-main').addClass('navbar-fixed-top');
   }
@@ -12,31 +13,31 @@ $(window).bind('scroll', function(e) {
 
 widthChange();
 $(window).on('resize', function(){
-      widthChange();
+  widthChange();
 });
 
 function widthChange() {
-	var isDesktop = window.innerWidth >= 768
-	var mobileElems = document.getElementsByClassName('mobile')
-	var desktopElems = document.getElementsByClassName('desktop')
+  var isDesktop = window.innerWidth >= 768
+  var mobileElems = document.getElementsByClassName('mobile')
+  var desktopElems = document.getElementsByClassName('desktop')
   var inlineDesktopElems = document.getElementsByClassName('inline-desktop')
-	for(var i = 0; i < mobileElems.length; i++) {
-		mobileElems[i].style.display = isDesktop ? 'none' : 'block';
-	}
-	for(var i = 0; i < desktopElems.length; i++) {
-		desktopElems[i].style.display = isDesktop? 'block' : 'none';
-	}
+  for(var i = 0; i < mobileElems.length; i++) {
+    mobileElems[i].style.display = isDesktop ? 'none' : 'block';
+  }
+  for(var i = 0; i < desktopElems.length; i++) {
+    desktopElems[i].style.display = isDesktop? 'block' : 'none';
+  }
   for(var i = 0; i < inlineDesktopElems.length; i++) {
     inlineDesktopElems[i].style.display = isDesktop? 'inline-block' : 'none';
   }
 }
 
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top-50
-        }, 1000, 'swing');
-        event.preventDefault();
-    });
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top-50
+    }, 1000, 'swing');
+    event.preventDefault();
+  });
 });
